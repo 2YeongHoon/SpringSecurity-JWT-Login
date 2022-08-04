a568d4ab09e9a713f79dfc5397427cbae999b394
yeonghoon.lee <djaakclsrn12@naver.com>
2022-08-04 22:31:40 +0900

    미사용 import삭제

--- src/main/java/com/jwt/auth/jwtpractice/JwtPracticeApplication.java
+++ src/main/java/com/jwt/auth/jwtpractice/JwtPracticeApplication.java
@@ -2,9 +2,6 @@ package com.jwt.auth.jwtpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 
 @SpringBootApplication
 public class JwtPracticeApplication {
